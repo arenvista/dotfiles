0d80233f4f262c0a2dd289096a108343e6f6eb18
sybil <arenvista1@gmail.com>
2024-03-22 09:23:18 -0500

    inital dotfile commit

---
+++ symlinks.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Nvim setup
+ln -s ~/.dotfiles/nvim ~/.config
+#Hyprland setup
+ln -s ~/.dotfiles/Hyprland ~
+ln -s ~/.dotfiles/hypr ~/.config
+ln -s ~/.dotfiles/rofi ~ #hypr launch menu
