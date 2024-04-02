db1dfb3ee45858bcc9428ed60cda3263bf56ee31
sybil <arenvista1@gmail.com>
2024-04-01 21:45:32 -0400

    fixed new symlink w/ zsh

---
+++ arch/symlinks.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+stow -D hypr -t ~
+stow -D nvim -t ~
+stow -D kitty -t ~
+stow -D waybar -t ~
+stow -D tmux -t ~
+stow -D bash -t ~
+stow -D zsh -t ~
+
+stow tmux -t ~
+stow hypr -t ~
+stow nvim -t ~
+stow kitty -t ~
+stow waybar -t ~
+stow bash -t ~
+stow zsh -t ~
