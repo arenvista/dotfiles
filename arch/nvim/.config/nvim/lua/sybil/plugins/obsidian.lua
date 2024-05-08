d170195dffa6e052d37556dbf1151687042641d6
sybil <arenvista1@gmail.com>
2024-05-08 10:44:21 -0400

    added keybinds to obsidian

--- arch/nvim/.config/nvim/lua/sybil/plugins/obsidian.lua
+++ arch/nvim/.config/nvim/lua/sybil/plugins/obsidian.lua
@@ -38,4 +38,12 @@ return {
 
         -- see below for full list of options 👇
     },
+    keys = {
+        { "<leader>on", "<cmd>ObsidianNew<cr>", desc = "New Obsidian note", mode = "n" },
+        { "<leader>oo", "<cmd>ObsidianOpen<cr>", desc = "Search Obsidian notes", mode = "n" },
+        { "<leader>os", "<cmd>ObsidianQuickSwitch<cr>", desc = "Quick Switch", mode = "n" },
+        { "<leader>ob", "<cmd>ObsidianBacklinks<cr>", desc = "Show location list of backlinks", mode = "n" },
+        { "<leader>ot", "<cmd>ObsidianTemplate<cr>", desc = "Follow link under cursor", mode = "n" },
+        { "<leader>op", "<cmd>ObsidianPasteImg<cr>", desc = "Paste imate from clipboard under cursor", mode = "n" },
+    },
 }
