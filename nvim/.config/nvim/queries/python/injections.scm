96f9a139abd13f941cce7315bccaa38c3533f17b
sybil <arenvista1@gmail.com>
2024-07-01 09:45:15 -0400

    new plugins + patch fixes for updates

---
+++ nvim/.config/nvim/queries/python/injections.scm
@@ -0,0 +1,12 @@
+(call
+
+(attribute
+
+attribute: (identifier) @_attribute (#eq? @_attribute "execute"))
+
+(argument_list
+
+(string) @sql)
+(#set! injection.language "sql")
+(#set! injection.include-children)
+) 
