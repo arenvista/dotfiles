5c361c22919d0ae3eb0db853ff657cf6205d9ffd
sybil-thinker <arenvista1@gmail.com>
2024-09-25 20:42:28 -0400

    added plugins and tmux sessionizer

--- nvim/.config/nvim/lua/sybil/plugins/alpha.lua
+++ nvim/.config/nvim/lua/sybil/plugins/alpha.lua
@@ -23,7 +23,7 @@ return {
       dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>Neotree<CR>", { desc = " Neotree"}),
       dashboard.button("SPC ff", "󰱼 > Find File", "<cmd>Telescope find_files<CR>"),
       dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
-      dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
+      dashboard.button("wr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
       dashboard.button("q", " > Quit NVIM", "<cmd>qa<CR>"),
     }
 
