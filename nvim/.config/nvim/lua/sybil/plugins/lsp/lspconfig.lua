d50271ce6f9597358f9aff910bf13ce7b8836001
sybil-thinker <arenvista1@gmail.com>
2025-01-21 16:56:44 -0500

    global rename

--- nvim/.config/nvim/lua/sybil/plugins/lsp/lspconfig.lua
+++ nvim/.config/nvim/lua/sybil/plugins/lsp/lspconfig.lua
@@ -46,8 +46,8 @@ return {
                 opts.desc = "See available code actions"
                 keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
 
-                opts.desc = "Smart rename"
-                keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
+                opts.desc = "Rename across entire project"
+                keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts) -- rename across entire project
 
                 opts.desc = "Show buffer diagnostics"
                 keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
