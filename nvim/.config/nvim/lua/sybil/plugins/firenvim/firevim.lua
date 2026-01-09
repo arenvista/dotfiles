91461a1d6da31c922e50e6d62283311e1f3aa794
Aren Vista <arenvista1@gmail.com>
2026-01-08 19:07:02 -0500

    firenvim edits

--- nvim/.config/nvim/lua/sybil/plugins/firenvim/firevim.lua
+++ nvim/.config/nvim/lua/sybil/plugins/firenvim/firevim.lua
@@ -1,4 +1,14 @@
-return{
+return {
     'glacambre/firenvim',
     build = ":call firenvim#install(0)",
+    config = function()
+        vim.g.firenvim_config = {
+            localSettings = {
+                ['.*'] = {
+                    -- 'never' prevents firenvim from automatically starting
+                    takeover = 'never',
+                },
+            }
+        }
+    end
 }
