f7f0703f03df0ab67303497ce3dbe7e9166c76c2
Aren Vista <arenvista1@gmail.com>
2026-04-11 23:43:10 -0400

    fixed mismatch

--- stowables/nvim/.config/nvim/lua/sybil/plugins/lsp/vimtex.lua
+++ stowables/nvim/.config/nvim/lua/sybil/plugins/lsp/vimtex.lua
@@ -1,30 +1,17 @@
 return {
 	"lervag/vimtex",
 	lazy = false, -- We don't want to lazy load VimTeX
-	-- tag = "v2.15", -- Uncomment to pin to a specific release
 	init = function()
-		-- 1. PDF Viewer: Zathura is highly recommended for forward/backward search
 		vim.g.vimtex_view_method = "zathura"
-
-		-- Update time for faster CursorHold events (e.g., for live previewing or plugins)
 		vim.opt.updatetime = 100
-
-		-- 2. Compiler Settings: Use latexmk and enable continuous compilation
 		vim.g.vimtex_compiler_method = "latexmk"
-
-		-- 3. Quickfix Window: Filter out common/annoying LaTeX warnings
 		vim.g.vimtex_quickfix_ignore_filters = {
 			"Underfull",
 			"Overfull",
 			"specifier changed to",
 			"Token not allowed in a PDF string",
 		}
-
-		-- Prevent the quickfix window from automatically opening on compilation errors
 		vim.g.vimtex_quickfix_mode = 0
-
-		-- 4. Conceal Settings (Optional but highly recommended)
-		-- Turns things like \alpha into α or \frac{1}{2} into ½ visually in Neovim
 		vim.g.vimtex_syntax_conceal = {
 			accents = 1,
 			cites = 1,
@@ -36,8 +23,15 @@ return {
 			math_super_sub = 1,
 			symbols = 1,
 		}
+		-- Enable VimTeX folding
+		vim.g.vimtex_fold_enabled = 1
+		vim.g.vimtex_fold_types = {
+			comments = { enabled = true },
+			-- envs = { whitelist = { "figure", "table", "equation", "itemize", "enumerate" } },
+		}
 
-		-- Note: To make conceal work, you also need to set vim.opt.conceallevel = 2
-		-- somewhere in your general options (e.g., in your options.lua)
+		-- Neovim fold settings
+		vim.opt.foldenable = true
+		vim.opt.foldlevelstart = 0 -- Start with everything folded
 	end,
 }
---
+++ stowables/nvim/.config/nvim/lua/sybil/plugins/lsp/zen.lua
@@ -0,0 +1,7 @@
+return {
+	"folke/zen-mode.nvim",
+	opts = {
+		window = { width = 80 },
+		plugins = { twilight = { enabled = true } },
+	},
+}
