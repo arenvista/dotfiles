ffa3bbb1ac99b40d4eb882a71da61b6e0fc77349
sybil-thinker <arenvista1@gmail.com>
2026-08-08 16:21:09 -0400

    sys daemon for org-cal

--- utils/firefox/install_systemd_service.sh
+++ utils/firefox/install_systemd_service.sh
@@ -4,3 +4,7 @@ echo "Copying Files"
 cp ./systemd/org-sync.path $HOME/.config/systemd/user/
 cp ./systemd/org-sync.service $HOME/.config/systemd/user/
 
+
+systemctl --user daemon-reload
+systemctl --user enable --now org-sync.path
+journalctl --user -u org-sync.service -f
